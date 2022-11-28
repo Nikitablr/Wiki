6bdfeae356dc93ed1c8bd83a18e7876e9b8093dd
nikrud <89258843+nikrud@users.noreply.github.com>
2022-11-28 21:38:25 +0300

    upd

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 def test():
     browser = webdriver.Chrome()
     browser.get("https://en.wikipedia.org/wiki/Main_Page")
-    assert 'Wikipedia' in browser.title
+    assert 'Booking' in browser.title
     browser.quit()
 
 
