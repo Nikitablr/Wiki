2d94e45dcc35ba99c1bfd3cdb9127e7e9a9e9640
nikrud <89258843+nikrud@users.noreply.github.com>
2022-11-28 21:32:36 +0300

    upd

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-import  time
+
 
 def test():
-    chrome_options = Options()
-    browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    browser = webdriver.Chrome()
     browser.get("https://en.wikipedia.org/wiki/Main_Page")
     browser.quit()
 
