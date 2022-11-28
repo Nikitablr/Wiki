639fd18f7048287357a91e1f641426e648002640
nikrud <89258843+nikrud@users.noreply.github.com>
2022-11-28 21:36:45 +0300

    upd

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 def test():
     browser = webdriver.Chrome()
     browser.get("https://en.wikipedia.org/wiki/Main_Page")
+    assert 'Wikipedia' in browser.title
     browser.quit()
 
 
